0f2ec95c3a249906cfd396ee2682da0059c7d25c
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-11 12:34:23 +0200

    added ResendWidget

---
+++ lib/presentation/screens/verification/widgets/resend_code.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:harri_farm_app/app/widgets/app_text.dart';
+import 'package:harri_farm_app/business_logic/helpers/colors.dart';
+
+class ResendWidget extends StatefulWidget {
+  const ResendWidget({super.key});
+
+  @override
+  State<ResendWidget> createState() => _ResendWidgetState();
+}
+
+class _ResendWidgetState extends State<ResendWidget> {
+  int counter = 60;
+  Timer? timer;
+
+  @override
+  void initState() {
+    count();
+    super.initState();
+  }
+
+  void count() {
+    counter = 60;
+    setState(() {});
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      counter--;
+      setState(() {});
+      if (counter == 0) {
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (counter != 0) {
+      return RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "00:${counter.toString().padLeft(2, '0')}",
+              style: const TextStyle(
+                color: AppColors.gray,
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+    return GestureDetector(
+      onTap: count,
+      child: AppText(
+        title: "resend_code".tr(),
+        decoration: TextDecoration.underline,
+        color: AppColors.gray,
+        fontSize: 16,
+      ),
+    );
+  }
+}
