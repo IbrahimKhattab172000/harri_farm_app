328cd18ae348f3eda8b5478a000e2ae7410b85f2
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-18 10:25:59 +0200

    added ProductsDetailsType

---
+++ lib/features/product_details/widgets/product_details_type.dart
@@ -0,0 +1,32 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:harri_farm_app/helpers/colors.dart';
+import 'package:harri_farm_app/helpers/dimentions.dart';
+import 'package:harri_farm_app/widgets/app_text.dart';
+
+class ProductsDetailsType extends StatelessWidget {
+  const ProductsDetailsType({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        AppText(
+          title: "${"type".tr()} : ",
+          fontSize: 16,
+          color: AppColors.gray,
+          fontWeight: FontWeight.w700,
+        ),
+        SizedBox(width: 4.width),
+        const AppText(
+          title: "الطليان",
+          fontSize: 16,
+          color: AppColors.black,
+          fontWeight: FontWeight.w700,
+        ),
+      ],
+    );
+  }
+}
