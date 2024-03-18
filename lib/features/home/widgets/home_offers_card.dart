5782d2f39c218fa96c1a26ad62800fb773dbac98
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-18 11:53:53 +0200

    added listview

--- lib/features/home/widgets/home_offers_card.dart
+++ lib/features/home/widgets/home_offers_card.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:harri_farm_app/features/product_details/view/view.dart';
+import 'package:harri_farm_app/helpers/dimentions.dart';
 import 'package:harri_farm_app/helpers/routes.dart';
 import 'package:harri_farm_app/widgets/app_product_card.dart';
 
 class HomeOffersCards extends StatelessWidget {
   const HomeOffersCards({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        AppProductCard(
-          onTap: () => RouteUtils.navigateTo(const ProductDetailsView()),
-        ),
-        AppProductCard(
-          onTap: () => RouteUtils.navigateTo(const ProductDetailsView()),
-        ),
-      ],
+    return Container(
+      constraints: const BoxConstraints(maxHeight: 300),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return AppProductCard(
+            onTap: () => RouteUtils.navigateTo(const ProductDetailsView()),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return SizedBox(width: 44.width);
+        },
+      ),
     );
   }
 }
